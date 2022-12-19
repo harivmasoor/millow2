3d57b407a44cefb47bbf51a41851b1fa23b98ca6
harivmasoor <harimasoor@gmail.com>
2022-12-19 07:39:38 -0800

    this is millow 2

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+pragma solidity ^0.8.0;
+
 interface IERC721 {
     function transferFrom(
         address _from,
@@ -10,5 +12,19 @@ interface IERC721 {
 }
 
 contract Escrow {
-
+    address public nftAddress;
+    address payable public seller;
+    address public inspector;
+    address public lender;
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
 }
