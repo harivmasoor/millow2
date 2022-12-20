eb39ce6568c4797ce84dece76c1d358d64a81eb2
harivmasoor <harimasoor@gmail.com>
2022-12-19 16:24:12 -0800

    it works!

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,12 +6,51 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let buyer, seller
+    let realEstate, escrow 
 
-    it('saves the addresses', async() => {
+    beforeEach(async () => {
+        // Setup accounts
+        [buyer, seller, inspector, lender] = await ethers.getSigners()
+
+
+        // Deploy Real Estate
         const RealEstate = await ethers.getContractFactory('RealEstate')     
         realEstate = await RealEstate.deploy()   
 
-        console.log(realEstate.address)
+        // Mint
+        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/1.png")
+        await transaction.wait()
+
+        const Escrow = await ethers.getContractFactory('Escrow')
+        escrow = await Escrow.deploy(
+            realEstate.address,
+            seller.address,
+            inspector.address,
+            lender.address,
+        )
+    })
+    describe('Deployment', () => {
+        it('Returns NFT address', async () => {
+            const result = await escrow.nftAddress()
+            expect(result).to.be.equal(realEstate.address)
+        })
+    
+        it('Returns seller', async () => {
+            const result = await escrow.seller()
+            expect(result).to.be.equal(seller.address) 
+        })
+    
+        it('Returns NFT inspector', async () => {
+            const result = await escrow.inspector()
+            expect(result).to.be.equal(inspector.address)  
+        })
+    
+        it('Returns lender', async () => {
+            const result = await escrow.lender()
+            expect(result).to.be.equal(lender.address)  
+        })
+
     })
 
 })
